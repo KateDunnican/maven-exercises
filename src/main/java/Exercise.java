c9990f1bd45b739c4f23e6d8b26811ad3a55d8ac
Kate McKinney <kate.mckinney210@gmail.com>
2020-04-08 13:47:51 -0500

    finished exercise

---
+++ src/main/java/Exercise.java
@@ -0,0 +1,18 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter some input: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println(StringUtils.isNumeric(userInput));
+        System.out.println(StringUtils.swapCase(userInput));
+        System.out.println(StringUtils.reverse(userInput));
+
+    }
+}
